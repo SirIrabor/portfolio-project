962750056794072facc7ba8d748511a21137d18e
SirIrabor <michael.irabor6@gmail.com>
2021-05-17 14:46:30 +0100

    First Push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from '../src/components/Narbar';
+import Header from '../src/components/Header';
+
+
+
+function App() {
+  return (
+     <>
+    <Navbar/>
+    <Header/>
+    </>
+  
+  );
+}
+
+export default App; 
